bf376e109eba34d064f24a7bfd2cbc2ac1b2975f
Jeral Pineda <jeral.cris@gmail.com>
2025-01-18 20:57:38 -0600

    style: actualizando width del hero

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -3,8 +3,8 @@ import { Container } from "@/components/container";
 const Hero = () => {
   return (
     <Container>
-      <div className="w-full h-[150px] md:h-[400px] flex bg-cover bg-center bg-no-repeat justify-center items-center !gap-[6rem] bg-[url('/hero-background.webp')] rounded-3xl p-6 md:p-10">
-        <img src="/banner.webp" alt="banner" className="w-full md:w-[700px] h-auto" />
+      <div className="w-full h-[130px] md:h-[400px] flex bg-cover bg-center bg-no-repeat justify-center items-center !gap-[6rem] bg-[url('/hero-background.webp')] rounded-3xl p-6 md:p-10">
+        <img src="/banner.webp" alt="banner" className="w-[220px] md:w-[700px] h-auto" />
       </div>
     </Container>
   );
