e3768295343dac52f543384b07712cc46d654928
Jeral Pineda <jeral.cris@gmail.com>
2025-01-18 19:47:42 -0600

    fix: actualizando el height del hero

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -3,8 +3,8 @@ import { Container } from "@/components/container";
 const Hero = () => {
   return (
     <Container>
-      <div className="w-full h-[400px] md:h-[500px] flex bg-cover bg-center bg-no-repeat justify-center items-center !gap-[6rem] bg-[url('/hero-background.webp')] rounded-3xl p-6 md:p-10">
-        <img src="/banner.webp" alt="banner" className="w-full md:w-[900px] h-auto" />
+      <div className="w-full h-[350px] md:h-[340px] flex bg-cover bg-center bg-no-repeat justify-center items-center !gap-[6rem] bg-[url('/hero-background.webp')] rounded-3xl p-6 md:p-10">
+        <img src="/banner.webp" alt="banner" className="w-full md:w-[700px] h-auto" />
       </div>
     </Container>
   );
