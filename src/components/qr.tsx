2a918e748caf73bc20dc93d2b5687cd3b7dbdc73
Jeral Pineda <jeral.cris@gmail.com>
2025-01-18 21:01:31 -0600

    style: actualizando el mb del qr

--- src/components/qr.tsx
+++ src/components/qr.tsx
@@ -3,7 +3,7 @@ import { Container } from "@/components/container";
 const QR = () => {
   return (
     <Container className="flex flex-col items-center justify-center md:mb-[15rem] mb-10">
-      <div className="mb-6">
+      <div className="mb-4">
         <h2 className="text-balance text-h4 font-black tracking-tighter sm:text-h3 md:text-h1 xl:text-9xl py-3 text-center">
           ESCANÉAME
         </h2>
